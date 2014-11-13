0897a26eb6d474bd0c284950878e45dc2f5222b8
hassan mumin <hmumin10@gmail.com>
2014-11-12 22:34:32 -0600

    date change

--- db/migrate/20141112033000_chnage_date_in_workouts.rb
+++ db/migrate/20141112033000_chnage_date_in_workouts.rb
@@ -1,6 +1,6 @@
 class ChnageDateInWorkouts < ActiveRecord::Migration
   def up
-    change_column :workouts, :date, :date
+    change_column :workouts, :date, 'date USING CAST("date" AS date)'
   end
 
   def down
